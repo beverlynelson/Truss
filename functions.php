c46fb43de8f56193f362f6987a1b490c3ba3788b
Stubby <stubby@cogsprocket.net>
2011-08-20 11:32:00 -0500

    Cleaned up truss/truss.php comments.

--- functions.php
+++ functions.php
@@ -10,14 +10,14 @@
 * 	File Nmae: 		functions.php
 */
 
-// Basic WordPress options.
+// Basic WordPress options.  
+// These will be pulled into Truss framework and made for easier manipulation through the truss parent object.
 if (function_exists('add_theme_support')) {
 	add_theme_support('post-thumbnails');
 	add_image_size('index-categories', 150, 150, true);
 	add_image_size('page-single', 350, 350, true);
 }
-
-	require_once("include/trusswork.inc.php" );
+  require_once("truss/truss.php");
 
 // ------------------------------------------------------- //	
 
