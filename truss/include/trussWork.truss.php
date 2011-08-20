95a5d012cab51afb0237df61e0178ee252b55eb2
Stubby <stubby@cogsprocket.net>
2011-08-20 11:20:45 -0500

    trussWork is now set to automatically look for any plugin file as a *.truss.php file.

--- truss/include/trussWork.truss.php
+++ truss/include/trussWork.truss.php
@@ -39,7 +39,7 @@ class trussWork {
     
     $class = array_key_exists($name, $this->registeredObjects) ? $this->registeredObjects[$name] : ucfirst($name);  // Check the trussWork registry
     
-    $file = dirname(__FILE__) . "/$name.inc.php"; // set the file name for the registered object.
+    $file = dirname(__FILE__) . "/$name.truss.php"; // set the file name for the registered object.
     
     if (!isset(self::$name) && file_exists($file)) { // Check that the object is not currently in trussWork and that a file exists for that object
       // Add the object to trussWork and set itself to a child object of trussWork
